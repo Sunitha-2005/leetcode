8bccc504e87b0079b4d52c41e1824e8a7217717a
Sunitha-2005 <bhukyasunitha410@gmail.com>
2025-09-23 17:57:24 +0530

    Time: 4 ms (98.10%) | Memory: 45.2 MB (27.83%) - LeetSync

---
+++ 155-min-stack/min-stack.java
@@ -0,0 +1,53 @@
+class MinStack {
+
+    public MinStack() {
+    }
+    private class Node{
+        private int val;
+        private int min;
+        private Node next;
+
+        private Node(int val,int min,Node next){
+            this.val=val;
+            this.min=min;
+            this.next=next;
+        }
+        private Node(int val,int min){
+            this.val=val;
+            this.min=min;
+            this.next=null;
+        }
+    }
+    private Node head;
+    public void push(int val){
+        if(head==null){
+            head=new Node(val,val);
+        }else{
+            head=new Node(val,Math.min(val,head.min),head);
+        }
+    }
+    
+    public void pop() {
+        head=head.next;
+        
+    }
+    
+    public int top() {
+        return head.val;
+        
+    }
+    
+    public int getMin() {
+        return head.min;
+        
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
