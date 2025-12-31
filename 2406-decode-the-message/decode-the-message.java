3f849a753379fbd7fa426aad377fab7a9a3d0165
Sunitha-2005 <bhukyasunitha410@gmail.com>
2025-12-31 11:34:33 +0530

    Time: 63 ms (5.12%) | Memory: 47.1 MB (7.57%) - LeetSync

--- 2406-decode-the-message/decode-the-message.java
+++ 2406-decode-the-message/decode-the-message.java
@@ -2,27 +2,28 @@ import java.util.HashMap;
 
 class Solution {
     public String decodeMessage(String key, String message) {
-        HashMap<Character, Character> hashMap = new HashMap<>();
-        int temp = 97;
-        
-        for (int i = 0; i < key.length(); i++) {
-            char ch = key.charAt(i);
-            if (ch != ' ' && !hashMap.containsKey(ch)) {
-                hashMap.put(ch, (char) temp);  
-                temp++;  
+        HashMap<Character,Character>hm=new HashMap<>();
+        int temp=97;
+        for(int i=0;i<key.length();i++){
+            char ch=key.charAt(i);
+            if(ch!=' ' && !hm.containsKey(ch)){
+                hm.put(ch,(char)temp);
+                temp++;
             }
         }
-        StringBuilder ans = new StringBuilder();
-        for (int i = 0; i < message.length(); i++) {
-            char ch = message.charAt(i);  
-            if (ch != ' ') {
-                char val = hashMap.get(ch);
-                ans.append(val);
-            } else {
-                ans.append(' ');
+        String ans="";
+        for(int i=0;i<message.length();i++){
+            char ch=message.charAt(i);
+            System.out.println(ch);
+            if(ch!=' '){
+                char val=hm.get(ch);
+                ans=ans+val;
+            }
+            else{
+                ans+=' ';
             }
         }
-
-        return ans.toString();  
+        return ans;
     }
+            
 }
